a50fe35ae771731f226d280820d2c6b6aa2b6d85
yandex-passport-id <157625303+yandex-passport-id@users.noreply.github.com>
2024-02-10 20:29:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.log('test-script');
-console.log('test-script', new Date().toISOString());
 
-// Получаем все элементы <h4> и <a> внутри элемента с классом "footer-bottom"
-var elements = document.querySelectorAll('.footer-bottom h4, .footer-bottom a');
 
-// Применяем стиль к каждому найденному элементу
-elements.forEach(function(element) {
-    element.style.color = 'green';
+window.addEventListener('load', (event) => {
+    console.log('test-script');
+    // Получаем все элементы <h4> и <a> внутри элемента с классом "footer-bottom"
+    var elements = document.querySelectorAll('.footer-bottom h4, .footer-bottom a');
+    
+    // Применяем стиль к каждому найденному элементу
+    elements.forEach(function(element) {
+        element.style.color = 'green';
+    });
 });
