1192ce4f5b749507adef4298db10a71f49cbde68
yandex-passport-id <157625303+yandex-passport-id@users.noreply.github.com>
2024-02-10 20:25:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log('test-script');
 console.log('test-script', new Date().toISOString());
 
+// Получаем все элементы <h4> и <a> внутри элемента с классом "footer-bottom"
+var elements = document.querySelectorAll('.footer-bottom h4, .footer-bottom a');
+
+// Применяем стиль к каждому найденному элементу
+elements.forEach(function(element) {
+    element.style.color = 'green';
+});
